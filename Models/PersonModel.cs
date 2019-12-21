c148848b0558ae9310e9c2f10090d128328abba5
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-21 15:00:07 +0000

    Added Model.

---
+++ Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapp.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; } = 0;
+
+        public bool IsAlive { get; set; } = true;
+    }
+}
