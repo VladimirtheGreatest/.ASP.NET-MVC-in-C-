d39f2aa9193e3ea0f1f9ed02a3628c47e77c5ebf
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-23 15:07:54 +0000

    Added Controller and a new View for the Controller.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCapp.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListPeople()
+        {
+            //model
+            List<PersonModel> people = new List<PersonModel>();
+
+            //data, can get it from the database, classlibrary, sql or the rest api
+            people.Add(new PersonModel { FirstName = "Vladimir", LastName = "Doktorov", Age = 29 });
+            people.Add(new PersonModel { FirstName = "Jeremy", LastName = "Corbyn", Age = 69 });
+            people.Add(new PersonModel { FirstName = "Jessica", LastName = "James", Age = 39 });
+
+            return View(people);
+        }
+    }
+}
