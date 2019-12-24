336502bb9cce5866fb53fdb323bd6a82efccd33e
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-24 14:39:36 +0000

    Customer error handler

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -14,6 +14,7 @@ namespace MVCapp.Controllers
         {
             return View();
         }
+        // GET: People/PeopleList
         public ActionResult ListPeople()
         {
             //model
